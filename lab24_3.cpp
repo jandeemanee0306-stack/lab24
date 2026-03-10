c3aa37961855ef89fede07f441cda4948bf89814
Siriphum <Jandeemanee0306@gmail.com>
2026-03-10 21:43:23 +0700

    lab24_3.cpp completed

--- lab24_3.cpp
+++ lab24_3.cpp
@@ -3,6 +3,24 @@
 using namespace std;
 
 //Write function count() here.
+int count(int data[], int n){
+    int diff = 0;
+
+    for(int i = 0; i < n; i++){
+        bool found = false;
+
+        for(int j = 0; j < i; j++){
+            if(data[i] == data[j]){
+                found = true;
+                break;
+            }
+        }
+
+        if(!found) diff++;
+    }
+
+    return diff;
+}
 
 int main(){	
 	int data[] = {1,2,4,5,4,8,2,1,2,4,6,1,4,4,4,2,1,0,12}; 
